9817ed002d2c11465f8a27f30695de7a11064934
Lucas Gouvea da Silva Carvalho <lucas.carvalho@olist-mb1956.local>
2022-11-13 13:35:49 -0300

    feat: initializing guns

--- internal/api/guns/schemas.go
+++ internal/api/guns/schemas.go
@@ -1,7 +1,16 @@
 package guns
 
+import "strings"
+
 type IGun struct {
-	Id    string  `redis:"id"`
-	Name  string  `redis:"name"`
-	Price float64 `redis:"price"`
+	Id    string  `redis:"id" json:"id"`
+	Name  string  `redis:"name" json:"name"`
+	Price float64 `redis:"price" json:"price"`
+}
+
+func (gun IGun) GetHash() string {
+	var hash strings.Builder
+	hash.WriteString("gun:")
+	hash.WriteString(gun.Id)
+	return hash.String()
 }
--- internal/redis/index.go
+++ internal/redis/index.go
@@ -2,8 +2,8 @@ package redis
 
 import (
 	"context"
+	"fmt"
 	"os"
-	"strings"
 	"time"
 
 	Guns "go-api/internal/api/guns"
@@ -11,22 +11,21 @@ import (
 	"github.com/gomodule/redigo/redis"
 )
 
-var ctx = context.Background()
-
-var INFINITE_TTL time.Duration = 0
-
-func getHash(gun Guns.IGun) string {
-	var hash strings.Builder
-	hash.WriteString("gun:")
-	hash.WriteString(gun.Id)
-	return hash.String()
+type Rediser[T any] interface {
+	getConnection() redis.Conn
+	GetMany(hashes []string) []T
 }
 
-func getMany(connection redis.Conn, hashes []string) []Guns.IGun {
+var ctx = context.Background()
+var INFINITE_TTL time.Duration = 0
+var Connection redis.Conn = nil
+
+func GetMany[T any](hashes []string) []T {
 	var err error
 	var reply interface{}
 	var values []interface{}
-	var guns []Guns.IGun
+	var models []T
+	var connection = getConnection()
 
 	for _, hash := range hashes {
 		connection.Send("HGETALL", hash)
@@ -34,8 +33,8 @@ func getMany(connection redis.Conn, hashes []string) []Guns.IGun {
 
 	connection.Flush()
 
-	for i := 0; i < len(Guns.Guns); i++ {
-		var data Guns.IGun
+	for i := 0; i < len(hashes); i++ {
+		var data T
 		reply, err = connection.Receive()
 		if err != nil {
 			panic(err)
@@ -49,30 +48,30 @@ func getMany(connection redis.Conn, hashes []string) []Guns.IGun {
 			panic(err)
 		}
 
-		guns = append(guns, data)
+		models = append(models, data)
 	}
 
-	return guns
+	return models
 }
 
-func Connect() redis.Conn {
-	connection, error := redis.DialContext(ctx, "tcp", os.Getenv("REDIS_ADDRESS"))
-
-	if error != nil {
-		panic(error)
+func getConnection() redis.Conn {
+	if Connection == nil {
+		connection, error := redis.DialContext(ctx, "tcp", os.Getenv("REDIS_ADDRESS"))
+		if error != nil {
+			panic(error)
+		}
+		Connection = connection
 	}
-
-	return connection
+	return Connection
 }
 
 func Setup() {
-
 	var hashes []string
 	var err error = nil
-	connection := Connect()
+	connection := getConnection()
 
 	for _, gun := range Guns.Guns {
-		var hash = getHash(gun)
+		var hash = gun.GetHash()
 		hashes = append(hashes, hash)
 		connection.Send("HMSET", redis.Args{}.Add(hash).AddFlat(&gun)...)
 	}
@@ -86,8 +85,11 @@ func Setup() {
 		}
 	}
 
-	// TODO: move to somehwere -> var guns = getMany(connection, hashes)
+	var guns = GetMany[Guns.IGun](hashes)
+	fmt.Printf("Initialized guns: \n")
+	for _, gun := range guns {
+		fmt.Printf("%v\n", gun.Name)
+	}
 
 	connection.Close()
-
 }
